7ec24ed5b02a26543b3b2de8b1efbc2ab62fbd61
StephaniePetronella <77924060+StephaniePetronella@users.noreply.github.com>
2021-10-10 10:25:27 +0200

    Add files via upload

---
+++ javascriptFunction8/script.js
@@ -0,0 +1,27 @@
+const colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+let text = "";
+
+
+//while loop
+
+while(colors[i]){
+  text += colors[i];
+  i++;
+}
+
+//For loop
+
+for (let i = 0; i < colors.length; i++) {
+  text += colors[i];
+}
+
+// array methods
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.forEach] ; 
+
+/*
+1. 3 regels
+2. 1 regel
+3.araay methode is makkelijk en overzichtelijk
+4.If you want, you can only loop through the object's own property by using the hasOwnProperty () method.
